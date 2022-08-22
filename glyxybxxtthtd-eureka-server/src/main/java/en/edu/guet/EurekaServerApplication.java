07530ecb09d6cb9e8c75272fe0b2f9d8f75ee485
郭乐源 <936085132@qq.com>
2022-08-22 11:06:03 +0800

    20220822  11：05  桂林医学院报修系统后端接口初始化项目

---
+++ glyxybxxtthtd-eureka-server/src/main/java/en/edu/guet/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package en.edu.guet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author Devere19
+ * @Date 2022/8/21 17:08
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
